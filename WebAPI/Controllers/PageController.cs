1c94514762a0cd608945516a708b8b2e3b7e2865
Max Zadarnovskiy <maxzadarnovskiyy@gmail.com>
2023-10-28 14:09:00 +1030

    Default starting point

---
+++ WebAPI/Controllers/PageController.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Mime;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI;
+
+[ApiController]
+[Route("/")]
+public class PageController : ControllerBase
+{
+
+    [HttpGet("/", Name = "Home")]
+    public IResult Home()
+    {
+
+        return Results.Content("<h1>Hi</h1>", MediaTypeNames.Text.Html);
+    }
+
+}
