d0eea05ce373e8cc43094b817a6a352a09dcd087
Max Zadarnovskiy <maxzadarnovskiyy@gmail.com>
2023-10-28 13:51:06 +1030

    Defaults

---
+++ Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System.CodeDom.Compiler;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace HtmxDotnet;
+
+[ApiController]
+[Route("api/test")]
+public class TestController : ControllerBase
+{
+
+    [HttpGet("hello")]
+    public IResult HelloWorld()
+    {
+        return Results.Extensions.RazorSlice("/Views/FullView.cshtml", "Hello");
+    }
+
+}
